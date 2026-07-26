9335b27269087b7e0d760cf23ce522bab2825d76
Sumit Kumar <sumitku2938k@gmail.com>
2026-07-26 09:04:41 +0530

    Update BackToTop component: adjust framer-motion import for consistency

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion as Motion } from 'framer-motion'
+import { AnimatePresence, m as Motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import { easeOutExpo } from '../lib/animations'
 
